c715d0dd4e4bec244771f854a17571fd2460261b
francois_popieul <francois.popieul@hotmail.fr>
2025-10-08 13:20:47 +0200

    implemented shadcn

--- src/core/pages/home.tsx
+++ src/core/pages/home.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Home</div>;
+import { Button } from "@/components/ui/button";
+
+function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant="outline" size="sm">
+          Click me
+        </Button>
+      </div>
+    </>
+  );
 }
+
+export default Home;
