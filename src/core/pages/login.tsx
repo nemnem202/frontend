0bf5787b992306665b51831a2ab9d42bde56cc42
nemnem202 <naimelhabbas@gmail.com>
2025-10-08 16:44:08 +0200

    implemented stylesheets folder

--- src/core/pages/login.tsx
+++ src/core/pages/login.tsx
@@ -1,10 +1,12 @@
 import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 export default function Login() {
   return (
     <>
       <div>login page</div>
       <Button>Coucou</Button>
+      <Input placeholder="coucou" />
     </>
   );
 }
