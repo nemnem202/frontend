2f64b9f3d3410e3b23fd97042004a0688e8e5258
francois_popieul <francois.popieul@hotmail.fr>
2025-10-09 16:22:14 +0200

    added site page header

--- src/core/pages/market/home.tsx
+++ src/core/pages/market/home.tsx
@@ -1,4 +1,15 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/core/components/ui/sidebar";
+import { ApiService } from "@/services/api_service";
+import { Product } from "@/types/tables/product";
 function Home() {
+  // const response = await ApiService.get<Product[]>(headers:Headers, path: string);
+
   return (
     <>
       <div>Home page</div>
