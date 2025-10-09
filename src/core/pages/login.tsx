fb87747d20166fe84aec9cd9594f8b9dfd49ab39
nemnem202 <naimelhabbas@gmail.com>
2025-10-09 12:35:04 +0200

    corrected .env and set up api_service

--- src/core/pages/login.tsx
+++ src/core/pages/login.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { ApiService } from "@/services/api_service";
+import { Response } from "@/types/general/response";
 
 export default function Login() {
+  const fetchApi = async () => {
+    const headers = new Headers();
+    // headers.append("Content-Type", "application/json");
+    const response = await ApiService.get<Response>(headers, "/account/account");
+
+    console.log(response);
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
     <>
       <div>login page</div>
