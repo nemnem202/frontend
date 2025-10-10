1ba0973dfe81c166f88828361c9fe4c45e700aad
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-10 15:00:21 +0200

    Change input name on form to match type on backend

--- src/core/pages/market/form-new-product.tsx
+++ src/core/pages/market/form-new-product.tsx
@@ -10,42 +10,46 @@ import {
 import { Input } from "../../components/ui/input"
 import { Textarea } from "@/core/components/ui/textarea";
 import { Button } from "@/core/components/ui/button";
+import { Card, CardContent } from "@/core/components/ui/card";
 
 export default function FromNewProduct() {
 
     return (
-
-        <form>
-            <FieldSet>
-                <FieldLegend>Article's name</FieldLegend>
-                <FieldGroup>
-                    <Field>
-                        <FieldLabel htmlFor="name">Product's name</FieldLabel>
-                        <Input id="productName" placeholder="" required />
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="name">Product's description</FieldLabel>
-                        <Textarea id="productDescription" placeholder="" required />
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="name">Product's quantity in stock</FieldLabel>
-                        <Input id="productQuantity" placeholder="" type="number" required />
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="name">Product' price</FieldLabel>
-                        <Input id="productPrice" placeholder="" type="number" required />
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="name">Product' photos</FieldLabel>
-                        <Input id="productPhotos" placeholder="" type="file" required />
-                    </Field>
-                    <FieldSeparator />
-                    <Field orientation="horizontal">
-                        <Button type="submit">Post new product</Button>
-                    </Field>
-                </FieldGroup>
-            </FieldSet>
-        </form>
+            <Card className="">
+                <CardContent>
+                    <form>
+                        <FieldSet>
+                            <FieldLegend>Article's name</FieldLegend>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="name">Product's name</FieldLabel>
+                                    <Input id="productName" placeholder="" name="product_name" required />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="productDescription">Product's description</FieldLabel>
+                                    <Textarea id="productDescription" placeholder="" name="product_description" required />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="name">Product's quantity in stock</FieldLabel>
+                                    <Input id="productQuantity" placeholder="" type="number" name="available_quantity" required />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="name">Product' price</FieldLabel>
+                                    <Input id="productPrice" placeholder="" type="number" name="product_price" required />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="name">Product' photos</FieldLabel>
+                                    <Input id="productPhotos" placeholder="" type="file" required />
+                                </Field>
+                                <FieldSeparator />
+                                <Field orientation="horizontal">
+                                    <Button type="submit">Post new product</Button>
+                                </Field>
+                            </FieldGroup>
+                        </FieldSet>
+                    </form>
+                </CardContent>
+            </Card>
 
     )
 }
