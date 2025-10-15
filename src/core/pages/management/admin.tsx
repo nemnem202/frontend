d8b20d97d1df83ab8dcbb5ed46adbaa56db74255
nemnem202 <naimelhabbas@gmail.com>
2025-10-15 14:25:26 +0200

    set up overview of admin page

--- src/core/pages/management/admin.tsx
+++ src/core/pages/management/admin.tsx
@@ -1,5 +1,6 @@
 import { successToastProps } from "@/config/display/toasterProps";
 import { Button } from "@/core/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/core/components/ui/card";
 import {
   Field,
   FieldGroup,
@@ -9,13 +10,54 @@ import {
   FieldSet,
 } from "@/core/components/ui/field";
 import { Input } from "@/core/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/core/components/ui/table";
 import { ApiService } from "@/services/api_service";
+import { Account } from "@/types/tables/accounts";
 import { toast } from "sonner";
+import { faker } from "@faker-js/faker";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
 
 export default function Admin() {
+  const generate_accounts = (count: number): Account[] => {
+    const accounts: Account[] = [];
+    for (let i = 0; i < count; i++) {
+      accounts.push({
+        username: faker.internet.username(),
+        id: faker.number.int(),
+        number_of_reports: faker.number.int(),
+        password: faker.string.sample(),
+        suspended: Math.random() > 0.5,
+      });
+    }
+
+    return accounts;
+  };
+
   return (
     <div className="admin-page">
       <RegisterModoAccount setMessage={() => {}} />
+      <Card>
+        <CardHeader>
+          <CardTitle>Manage accounts</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ManageAccount title="modos" accounts={generate_accounts(10)} />
+          <ManageAccount title="users" accounts={generate_accounts(30)} />
+          <ManageAccount title="vendors" accounts={generate_accounts(10)} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
@@ -48,26 +90,70 @@ function RegisterModoAccount({
     }
   };
   return (
-    <form onSubmit={(e) => register(e)}>
-      <FieldGroup>
-        <FieldSet>
-          <FieldLegend>Create a modo account</FieldLegend>
+    <Card>
+      <CardHeader>
+        <CardTitle>Create a modo account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={(e) => register(e)}>
           <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="username">User-name</FieldLabel>
-              <Input id="username" name="username" placeholder="Evil Rabbit" required />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" name="password" type="password" required />
+            <FieldSet>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="username">User-name</FieldLabel>
+                  <Input id="username" name="username" placeholder="Evil Rabbit" required />
+                </Field>
+                <Field>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input id="password" name="password" type="password" required />
+                </Field>
+              </FieldGroup>
+            </FieldSet>
+            <FieldSeparator />
+            <Field orientation="horizontal">
+              <Button type="submit">Submit</Button>
             </Field>
           </FieldGroup>
-        </FieldSet>
-        <FieldSeparator />
-        <Field orientation="horizontal">
-          <Button type="submit">Submit</Button>
-        </Field>
-      </FieldGroup>
-    </form>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ManageAccount({ accounts, title }: { accounts: Account[]; title: string }) {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>{title}</AccordionTrigger>
+        <AccordionContent>
+          <Table className="w-[700px]">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[300px]">Username</TableHead>
+                <TableHead>Number of reports</TableHead>
+                <TableHead className="text-right">Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {accounts.map((a) => (
+                <TableRow>
+                  <TableCell className="font-medium">{a.username}</TableCell>
+                  <TableCell>{a.number_of_reports}</TableCell>
+                  <TableCell className="text-right">
+                    {!a.suspended ? (
+                      <Button className="w-[100px]">Active</Button>
+                    ) : (
+                      <Button variant={"destructive"} className="w-[100px]">
+                        Suspended
+                      </Button>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
